60c8a16af0464e8104b9b0100f03d5b2f81c656c
Sacchidanand Pandey <saurabhpnd9315@gmail.com>
2025-03-01 00:35:23 +0530

    Create Question 30: Bitwise_Check if Number is Power of 2.c
    Write a program to check if a given integer n is a power of 2 using bitwise operators.
    
    Input Format
    
    A single integer n.
    Constraints
    
    −10^9 ≤ n ≤ 10^9
    
    Output Format
    
    Print "Yes" if n is a power of 2.
    Print "No" otherwise.

---
+++ Question 30: Bitwise_Check if Number is Power of 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+    scanf("%d",&n);
+    
+    if(((n)&(n-1))==0 && n!=0){
+        printf("Yes");
+        
+    }
+       else{
+           printf("No");
+       }
+}
